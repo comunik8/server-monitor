650a48622a8e16e368fa7a7786e8a22c2519ec2c
KrazyBot <elliotwarren17@gmail.com>
2020-01-24 16:54:10 +0000

    prep for disk usage

--- monitor.sh
+++ monitor.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 cpuu=$[100-$(vmstat 1 2|tail -1|awk '{print $15}')] #gives 2 diget number to represent overall cpu usage (takes 1 second to run)
+memu=$(free | awk ' /Mem:/ {print $3/$4}') #gives mem usage in % to 3 decimal places
+df -h | awk 'NR>1 {print $5}' | while read line; do
+  if [ "${line::-1}" -gt "80" ]; then
+    echo "test"
+  fi
+done
